e345ed1733646ebdca058f11a4300aa680c2c90c
Griffin Amaral <griffin.amaral98@gmail.com>
2023-10-24 17:17:14 -0400

    Configure React app for deployment to GitHub Pages

--- src/index.js
+++ src/index.js
@@ -1,17 +1,102 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const profileData = {
+  name: "Griffin Amaral",
+  description: "Aspiring full-stack web developer and self taught student",
+  skills: [
+    {
+      skill: "HTML+CSS",
+      level: "Advanced",
+      color: "#2662EA",
+    },
+    {
+      skill: "JavaScript",
+      level: "Advanced",
+      color: "#EFD81D",
+    },
+    {
+      skill: "Web Design",
+      level: "Advanced",
+      color: "#C3DCAF",
+    },
+    {
+      skill: "Git and Github",
+      level: "Intermediate",
+      color: "#E84F33",
+    },
+    {
+      skill: "React",
+      level: "Intermediate",
+      color: "#60DAFB",
+    },
+    {
+      skill: "Python",
+      level: "Intermediate",
+      color: "#4584B6",
+    },
+    {
+      skill: "Jest",
+      level: "Beginner",
+      color: "#C63D14",
+    },
+  ],
+};
+
+export const Avatar = () => {
+  return <img src="avatar.jpg" alt="My wife and I" className="avatar" />;
+};
+
+export const Intro = (props) => {
+  return (
+    <div className="avatar">
+      <h1>{props.name}</h1>
+      <p>{props.description}</p>
+    </div>
+  );
+};
+
+export const SkillList = (props) => {
+  const skills = props.skills;
+
+  return (
+    <div>
+      <ul className="skill-list">
+        {skills.map((skillObj) => (
+          <li
+            key={skillObj.name}
+            className="skill"
+            style={{ backgroundColor: skillObj.color }}
+          >
+            {skillObj.skill}{" "}
+            {skillObj.level === "Advanced"
+              ? "💪"
+              : skillObj.level === "Intermediate"
+              ? "👍"
+              : "🐵"}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export const App = () => {
+  return (
+    <div className="card">
+      <Avatar />
+      <div className="data">
+        <Intro name={profileData.name} description={profileData.description} />
+        <SkillList skills={profileData.skills} />
+      </div>
+    </div>
+  );
+};
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
